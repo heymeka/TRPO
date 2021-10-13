a7beb62cfdc83beb307fe6496f185a8f4769a8f2
Viktor <375291163332@mail.ru>
2021-10-13 13:30:39 +0300

    Add multiply function Refactor to class Refactor output

--- 2021_10_13/main.cpp
+++ 2021_10_13/main.cpp
@@ -7,45 +7,92 @@
       ---------------------------------------------------
                                             			**/
 #include <iostream>
+#include <iomanip>
 #include <ctime>
 using std::cout;
 using std::cin;
+using std::setw;
+const int SPACE = 5;
 
-struct matrix {
+class matrix {
+ private:
   int** values;
-  int row = 0;
-  int col = 0;
+  int row;
+  int col;
+ public:
+  matrix() {
+    row = 0;
+    col = 0;
+  }
+  void init(int _row, int _col);
+  void init();
+  void print();
+  void erase();
+  matrix operator*(matrix& matr2);
 };
 
-matrix* initMatrix();
-
-void printMatrix(matrix* matr);
-
 double getSum(int* array, int& array_size);
-
-matrix* multiplyMatrix(matrix* matr1, matrix* matr2);
 
 void deleteDuplicateItems(int* array, int& array_size);
 
 double getMedium(int* array, int& array_size);
 
-void eraseMatrix(matrix* matr);
+void taskPrint();
+
+void taskSum();
+
+void taskMultiply();
 
 int main() {
   srand(time(NULL));
-  matrix* matr1;
-  matr1 = initMatrix();
-  printMatrix(matr1);
-
-  eraseMatrix(matr1);
+  taskPrint();
+  taskSum();
+  taskMultiply();
   system("pause");
   return 0;
 }
 
-matrix* initMatrix() {
-  matrix* matr = new matrix;
-  int row;
-  int col;
+void taskPrint() {
+  cout << "Task 1: Print N*K Matrix\n\n";
+  matrix matr1;
+  matr1.init();
+  cout << "Matrix:\n";
+  matr1.print();
+  matr1.erase();
+  cout << "\n\n";
+}
+
+void taskSum() {
+  cout << "Task2: Get the sum and average value of the elements in the array\n";
+
+  cout << "\n\n";
+}
+
+void taskMultiply() {
+  cout << "Task3: Matrix multiply\n\n";
+  matrix matr1;
+  matrix matr2;
+  cout << "First Matrix\n\n";
+  matr1.init();
+  cout << "\n";
+  matr1.print();
+
+  cout << "Second Matrix\n\n";
+  matr2.init();
+  cout << "\n";
+  matr2.print();
+
+  cout << "\nResult:\n";
+  matrix matr3 = matr1 * matr2;
+  matr3.print();
+
+  matr1.erase();
+  matr2.erase();
+  matr3.erase();
+  cout << "\n\n";
+}
+
+void matrix::init() {
   cout << "Enter matrix size:\n";
   cout << "row count = ";
   cin >> row;
@@ -62,54 +109,85 @@ matrix* initMatrix() {
   int interval_start;
   int interval_finish;
   cout << "Enter the number generation interval\n";
-  cout << "Generate\nfrom ";
+  cout << "Min value = ";
   cin >> interval_start;
-  cout << "to ";
+  cout << "Max value = ";
   cin >> interval_finish;
 
   while (interval_start >= interval_finish) {
     cout << "Interval input error\n";
-    cout << "Generate\nfrom ";
+    cout << "Min value = ";
     cin >> interval_start;
-    cout << "to ";
+    cout << "Max value = ";
     cin >> interval_finish;
-
   }
   mod = (interval_finish - interval_start + 1);
 
-  matr->row = row;
-  matr->col = col;
-  matr->values = new int* [row];
+  values = new int* [row];
   for (int i = 0; i < row; i++) {
-    matr->values[i] = new int[col];
+    values[i] = new int[col];
   }
 
   for (int r = 0; r < row; r++) {
     for (int c = 0; c < col; c++) {
-      matr->values[r][c] = rand() % mod + interval_start;
+      values[r][c] = rand() % mod + interval_start;
     }
   }
-  return matr;
 }
 
-void eraseMatrix(matrix* matr) {
-  for (int i = 0; i < matr->row; i++) {
-    delete[] matr->values[i];
+void matrix::init(int _row, int _col) {
+  if (_row < 1) {
+    _row = 1;
   }
-  delete[] matr->values;
+  if (_col < 1) {
+    _col = 1;
+  }
+  row = _row;
+  col = _col;
+  values = new int* [row];
+  for (int i = 0; i < row; i++) {
+    values[i] = new int[col];
+  }
 }
 
+void matrix::erase() {
+  for (int i = 0; i < row; i++) {
+    delete[] values[i];
+  }
+  delete[] values;
+}
 
-void printMatrix(matrix* matr) {
-  if(matr -> row == 0 || matr -> col == 0) {
+void matrix::print() {
+  if (row < 1 || col < 1) {
     cout << "Error. Invalid matrix to print\n";
     return;
   }
-  cout << "Matrix:\n";
-  for(int r = 0; r < matr -> row; r++) {
-    for(int c = 0; c < matr -> col; c++) {
-      cout << matr -> values[r][c] << ' ';
+  for (int r = 0; r < row; r++) {
+    for (int c = 0; c < col; c++) {
+      cout << setw(SPACE) << values[r][c] << ' ';
     }
     cout << '\n';
   }
+}
+
+matrix matrix::operator*(matrix& matr2) {
+  matrix result;
+  if (row < 1 || col < 1 || matr2.row < 1 || matr2.col < 1
+      || col != matr2.row) {
+    cout << "Error. Invalid matrix to multiply\n";
+    result.init(1, 1);
+    result.values[0][0] = -(1 << 25);
+    return result;
+  }
+  result.init(row, matr2.col);
+  for (int r = 0; r < row; r++) {
+    for (int c = 0; c < matr2.col; c++) {
+      int buf_result = 0;
+      for (int check = 0; check < col; check++) {
+        buf_result += values[r][check] * matr2.values[check][c];
+      }
+      result.values[r][c] = buf_result;
+    }
+  }
+  return result;
 }
