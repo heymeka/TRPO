b8b29a01e65744c3f4a557b7430de5d14a427ec0
Viktor <375291163332@mail.ru>
2021-10-21 15:08:59 +0300

    Add ValueToString function for int and double

---
+++ 2021_10_20/main.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <algorithm>
+using std::string;
+using std::cin;
+using std::cout;
+
+string getStringFromValue(long long value, int numbers_count = (1 << 27));
+string getStringFromValue(double value);
+
+int main() {
+  // cout << getStringFromValue((long long) 123) << '\n';
+  cout << getStringFromValue(1. / 7) << '\n';
+
+  system("pause");
+  return 0;
+}
+
+string getStringFromValue(long long value, int numbers_count) {
+  cout << numbers_count << '\n';
+  string raw_value;
+  if (value == 0) {
+    raw_value = "0";
+    return raw_value;
+  }
+  bool is_negative = (value < 0);
+  if (value < 0) {
+    value *= -1;
+  }
+  while (value != 0 && numbers_count > 0) {
+    raw_value += '0' + value % 10;
+    value /= 10;
+    numbers_count--;
+  }
+
+  if (is_negative) {
+    raw_value += "-";
+  }
+  reverse(raw_value.begin(), raw_value.end());
+  return raw_value;
+}
+
+string getStringFromValue(double value) {
+  string raw_value;
+
+  if (value == 0) {
+    raw_value = "0";
+    return raw_value;
+  }
+
+  bool is_negative = false;
+  if (value < 0) {
+    value *= -1;
+    is_negative = true;
+  }
+  const double MIN_VALUE = 1;
+  const double MAX_VALUE = 10;
+  int last_zero_count = 0;
+  int first_zero_count = 0;
+  while (value < MIN_VALUE) {
+    value *= 10;
+    first_zero_count++;
+  }
+  while (value >= MAX_VALUE) {
+    value /= 10;
+    last_zero_count++;
+  }
+
+  int digit;
+  int digits_count = 17;
+  while (digits_count != 0) {
+    digit = value;
+    raw_value += digit + '0';
+    value = (value - digit) * 10;
+    digits_count--;
+  }
+  string first_zeroes;
+  while (first_zero_count != 0) {
+    first_zeroes += "0";
+    first_zero_count--;
+  }
+  string last_zeroes;
+  int dot_index = 1 + last_zero_count;
+  while (last_zero_count != 0) {
+    last_zeroes += "0";
+    last_zero_count--;
+  }
+  raw_value = first_zeroes + raw_value + last_zeroes;
+  raw_value.insert(dot_index, ".");
+  if(is_negative) {
+    raw_value.insert(0, "-");
+  }
+  return raw_value;
+}
